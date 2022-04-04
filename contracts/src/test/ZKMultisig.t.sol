615b1094d13c07a73930381e03b5f980d237166d
arnaucube <root@arnaucube.com>
2022-04-04 19:39:52 +0200

    Init contracts with Foundry

---
+++ contracts/src/test/ZKMultisig.t.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.8.10;
+
+import "ds-test/test.sol";
+import "src/ZKMultisig.sol";
+
+contract ZKMultisigTest is DSTest {
+    function setUp() public {}
+
+    function testExample() public {
+        assertTrue(true);
+    }
+
+}
