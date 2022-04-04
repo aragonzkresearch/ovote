21110f8d7bf6288b242b0b5761493b8439d6d1d8
arnaucube <root@arnaucube.com>
2022-04-04 19:52:23 +0200

    Add contract initial version of newProcess method

--- contracts/src/ZKMultisig.sol
+++ contracts/src/ZKMultisig.sol
@@ -1,4 +1,43 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity 0.8.10;
 
-contract ZKMultisig {}
+// WARNING: This code is WIP, in early stages.
+
+contract ZKMultisig {
+	struct Process {
+		address creator; // the sender of the tx that created the process
+		uint256 censusRoot;
+		// next 6 values use 209 bits, so they fit in a single 256 storage slot
+		uint64 censusSize;
+		uint64 ethEndBlockNum;
+		uint64 resultsPublishingWindow;
+		uint8 minParticipation;
+		uint8 minPositiveVotes;
+		bool closed;
+	}
+
+	uint256 public lastProcessID; // initialized at 0
+	mapping(uint256 => Process) public processes;
+
+	// newProcess stores Process struct into the processes mapping
+	function newProcess(
+		uint256 censusRoot,
+		uint64 censusSize,
+		uint64 ethEndBlockNum,
+		uint64 resultsPublichingWindow,
+		uint8 minParticipation,
+		uint8 minPositiveVotes
+	) public returns (uint256) {
+		require(minPositiveVotes <= censusSize, "minPositiveVotes <= censusSize");
+
+		processes[lastProcessID +1] = Process(msg.sender, censusRoot,
+				  censusSize, ethEndBlockNum,
+				  resultsPublichingWindow, minParticipation,
+				  minPositiveVotes, false);
+
+		// assume that we use solidity>=0.8, which prevents overflow with normal addition
+		lastProcessID += 1;
+
+		return lastProcessID;
+	}
+}
--- contracts/src/test/ZKMultisig.t.sol
+++ contracts/src/test/ZKMultisig.t.sol
@@ -5,10 +5,55 @@ import "ds-test/test.sol";
 import "src/ZKMultisig.sol";
 
 contract ZKMultisigTest is DSTest {
-    function setUp() public {}
+    ZKMultisig zkmultisig;
 
-    function testExample() public {
-        assertTrue(true);
+    function setUp() public {
+        zkmultisig = new ZKMultisig();
     }
 
+    function testNewProcess() public {
+        uint256 censusRoot = 1111;
+        uint64 censusSize = 1000;
+        uint64 ethEndBlockNum = 1000;
+        uint64 resultsPublishingWindow = 100;
+        uint8 minParticipation = 10;
+        uint8 minPositiveVotes = 60;
+
+        // create a 1st process with the parameters
+        zkmultisig.newProcess(censusRoot, censusSize, ethEndBlockNum,
+                              resultsPublishingWindow, minParticipation,
+                              minPositiveVotes);
+
+        // get the process with id=1
+        (address _creator, uint256 _censusRoot, uint64 _censusSize,
+         uint64 _ethEndBlockNum, uint64 _resultsPublishingWindow, uint8
+         _minParticipation, uint8 _minPositiveVotes, bool closed) = zkmultisig.processes(1);
+
+         // address expectedCreator = 0xb4c79daB8f259C7Aee6E5b2Aa729821864227e84;
+        assertEq(_creator, 0xb4c79daB8f259C7Aee6E5b2Aa729821864227e84);
+        assertEq(_censusRoot, censusRoot);
+        assertEq(_censusSize, censusSize);
+        assertEq(_ethEndBlockNum, ethEndBlockNum);
+        assertEq(_resultsPublishingWindow, resultsPublishingWindow);
+        assertEq(_minParticipation, minParticipation);
+        assertEq(_minPositiveVotes, minPositiveVotes);
+
+
+        // create a 2nd process with the exact same parameters
+        zkmultisig.newProcess(censusRoot, censusSize, ethEndBlockNum,
+                              resultsPublishingWindow, minParticipation,
+                              minPositiveVotes);
+        // get the process with id=2
+        (_creator, _censusRoot, _censusSize, _ethEndBlockNum,
+         _resultsPublishingWindow, _minParticipation, _minPositiveVotes, closed) =
+             zkmultisig.processes(2);
+
+        assertEq(_creator, 0xb4c79daB8f259C7Aee6E5b2Aa729821864227e84);
+        assertEq(_censusRoot, censusRoot);
+        assertEq(_censusSize, censusSize);
+        assertEq(_ethEndBlockNum, ethEndBlockNum);
+        assertEq(_resultsPublishingWindow, resultsPublishingWindow);
+        assertEq(_minParticipation, minParticipation);
+        assertEq(_minPositiveVotes, minPositiveVotes);
+    }
 }
