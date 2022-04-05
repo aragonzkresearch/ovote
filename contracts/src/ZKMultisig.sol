8d8f5a2a695a428cc65b90cae7478c39d313bc3a
arnaucube <root@arnaucube.com>
2022-04-05 10:27:33 +0200

    Add contract closeProcess method

--- contracts/src/ZKMultisig.sol
+++ contracts/src/ZKMultisig.sol
@@ -3,10 +3,13 @@ pragma solidity 0.8.10;
 
 // WARNING: This code is WIP, in early stages.
 
+/// @title ZKMultisig
 contract ZKMultisig {
 	struct Process {
 		address creator; // the sender of the tx that created the process
+		uint256 transactionHash;
 		uint256 censusRoot;
+
 		// next 6 values use 209 bits, so they fit in a single 256 storage slot
 		uint64 censusSize;
 		uint64 ethEndBlockNum;
@@ -15,12 +18,27 @@ contract ZKMultisig {
 		uint8 minPositiveVotes;
 		bool closed;
 	}
+	struct Result {
+		address publisher;
+		uint256 result;
+		uint256 nVotes;
+	}
 
 	uint256 public lastProcessID; // initialized at 0
 	mapping(uint256 => Process) public processes;
+	mapping(uint256 => Result) public results;
 
-	// newProcess stores Process struct into the processes mapping
+	event EventProcessCreated(address creator, uint256 id,uint256
+				  transactionHash,  uint256 censusRoot, uint64
+				  censusSize, uint64 ethEndBlockNum, uint64
+				  resultsPublichingWindow, uint8
+				  minParticipation, uint8 minPositiveVotes);
+	event EventProcessClosed(address caller, uint256 id, bool success);
+
+
+	// @notice stores a new Process into the processes mapping
 	function newProcess(
+		uint256 transactionHash,
 		uint256 censusRoot,
 		uint64 censusSize,
 		uint64 ethEndBlockNum,
@@ -30,14 +48,45 @@ contract ZKMultisig {
 	) public returns (uint256) {
 		require(minPositiveVotes <= censusSize, "minPositiveVotes <= censusSize");
 
-		processes[lastProcessID +1] = Process(msg.sender, censusRoot,
-				  censusSize, ethEndBlockNum,
-				  resultsPublichingWindow, minParticipation,
-				  minPositiveVotes, false);
+		processes[lastProcessID +1] = Process(msg.sender, transactionHash,
+				censusRoot, censusSize, ethEndBlockNum,
+				resultsPublichingWindow, minParticipation,
+				minPositiveVotes, false);
 
-		// assume that we use solidity>=0.8, which prevents overflow with normal addition
+		// assume that we use solidity versiont >=0.8, which prevents
+		// overflow with normal addition
 		lastProcessID += 1;
+
+		emit EventProcessCreated(msg.sender, lastProcessID, transactionHash,
+					 censusRoot, censusSize, ethEndBlockNum,
+					 resultsPublichingWindow,
+					 minParticipation, minPositiveVotes);
 
 		return lastProcessID;
 	}
+
+	// @notice closes the process for the given id
+	function closeProcess(uint256 id) public {
+		// get process by id
+		Process storage process = processes[id];
+		require(process.closed == false, "process already closed");
+
+		// close process (in storage)
+		process.closed = true;
+
+
+		// get result by process id
+		Result storage result;
+		result = results[id];
+
+		// minParticipationNumber =  // TODO % of minParticipation & minPositiveVotes
+
+		require(block.number >= process.ethEndBlockNum + process.resultsPublishingWindow,
+			"process.ethEndBlockNum not reached yet");
+		if (result.result >= process.minPositiveVotes) {
+			// TODO call the tx of the transactionHash
+		}
+
+		emit EventProcessClosed(msg.sender, id, true);
+	} 
 }
--- contracts/src/test/ZKMultisig.t.sol
+++ contracts/src/test/ZKMultisig.t.sol
@@ -4,6 +4,11 @@ pragma solidity 0.8.10;
 import "ds-test/test.sol";
 import "src/ZKMultisig.sol";
 
+interface CheatCodes {
+    function roll(uint256) external;
+    function expectRevert(bytes calldata) external;
+}
+
 contract ZKMultisigTest is DSTest {
     ZKMultisig zkmultisig;
 
@@ -12,48 +17,74 @@ contract ZKMultisigTest is DSTest {
     }
 
     function testNewProcess() public {
-        uint256 censusRoot = 1111;
-        uint64 censusSize = 1000;
-        uint64 ethEndBlockNum = 1000;
-        uint64 resultsPublishingWindow = 100;
-        uint8 minParticipation = 10;
-        uint8 minPositiveVotes = 60;
+        // create a 1st process
+        uint256 id = zkmultisig.newProcess(2222, 1111, 1000, 1000, 100, 10, 60);
+        assertEq(id, 1);
 
-        // create a 1st process with the parameters
-        zkmultisig.newProcess(censusRoot, censusSize, ethEndBlockNum,
-                              resultsPublishingWindow, minParticipation,
-                              minPositiveVotes);
-
+        { // scope for process 1, avoids stack too deep errors
         // get the process with id=1
-        (address _creator, uint256 _censusRoot, uint64 _censusSize,
-         uint64 _ethEndBlockNum, uint64 _resultsPublishingWindow, uint8
-         _minParticipation, uint8 _minPositiveVotes, bool closed) = zkmultisig.processes(1);
+        (address _creator, uint256 _transactionHash, uint256 _censusRoot,
+         uint64 _censusSize, uint64 _ethEndBlockNum, uint64
+         _resultsPublishingWindow, uint8 _minParticipation, uint8
+         _minPositiveVotes, bool _closed) = zkmultisig.processes(1);
 
-         // address expectedCreator = 0xb4c79daB8f259C7Aee6E5b2Aa729821864227e84;
-        assertEq(_creator, 0xb4c79daB8f259C7Aee6E5b2Aa729821864227e84);
-        assertEq(_censusRoot, censusRoot);
-        assertEq(_censusSize, censusSize);
-        assertEq(_ethEndBlockNum, ethEndBlockNum);
-        assertEq(_resultsPublishingWindow, resultsPublishingWindow);
-        assertEq(_minParticipation, minParticipation);
-        assertEq(_minPositiveVotes, minPositiveVotes);
+        assertEq(_creator, 0xb4c79daB8f259C7Aee6E5b2Aa729821864227e84); // address of creator
+        assertEq(_transactionHash, 2222);
+        assertEq(_censusRoot, 1111);
+        assertEq(_censusSize, 1000);
+        assertEq(_ethEndBlockNum, 1000);
+        assertEq(_resultsPublishingWindow, 100);
+        assertEq(_minParticipation, 10);
+        assertEq(_minPositiveVotes, 60);
+        assertTrue(!_closed);
+        }
 
 
         // create a 2nd process with the exact same parameters
-        zkmultisig.newProcess(censusRoot, censusSize, ethEndBlockNum,
-                              resultsPublishingWindow, minParticipation,
-                              minPositiveVotes);
+        id = zkmultisig.newProcess(2222, 1111, 1000, 1000, 100, 10, 60);
+        assertEq(id, 2);
         // get the process with id=2
-        (_creator, _censusRoot, _censusSize, _ethEndBlockNum,
-         _resultsPublishingWindow, _minParticipation, _minPositiveVotes, closed) =
-             zkmultisig.processes(2);
+        { // scope for process 1, avoids stack too deep errors
+        (address _creator, uint256 _transactionHash, uint256 _censusRoot,
+         uint64 _censusSize, uint64 _ethEndBlockNum, uint64
+         _resultsPublishingWindow, uint8 _minParticipation, uint8
+         _minPositiveVotes, bool _closed) = zkmultisig.processes(2);
 
         assertEq(_creator, 0xb4c79daB8f259C7Aee6E5b2Aa729821864227e84);
-        assertEq(_censusRoot, censusRoot);
-        assertEq(_censusSize, censusSize);
-        assertEq(_ethEndBlockNum, ethEndBlockNum);
-        assertEq(_resultsPublishingWindow, resultsPublishingWindow);
-        assertEq(_minParticipation, minParticipation);
-        assertEq(_minPositiveVotes, minPositiveVotes);
+        assertEq(_transactionHash, 2222);
+        assertEq(_censusRoot, 1111);
+        assertEq(_censusSize, 1000);
+        assertEq(_ethEndBlockNum, 1000);
+        assertEq(_resultsPublishingWindow, 100);
+        assertEq(_minParticipation, 10);
+        assertEq(_minPositiveVotes, 60);
+        assertTrue(!_closed);
+        }
+    }
+
+    function testCloseProcess() public {
+        CheatCodes cheats = CheatCodes(HEVM_ADDRESS);
+
+        // create a 1st process
+        uint256 id = zkmultisig.newProcess(2222, 1111, 1000, 1000, 100, 10, 60);
+        assertEq(id, 1);
+
+        // publish result
+        // zkmultisig.publishResult(id, 204, 300); // WIP
+
+        // try to close process, but expect revert
+        cheats.roll(1099);
+        cheats.expectRevert(bytes("process.ethEndBlockNum not reached yet"));
+        zkmultisig.closeProcess(1);
+
+        (,,,,,,,, bool _closed) = zkmultisig.processes(1);
+        assertTrue(!_closed);
+
+        // close process
+        cheats.roll(1100);
+        zkmultisig.closeProcess(1);
+
+        (,,,,,,,, _closed) = zkmultisig.processes(1);
+        assertTrue(_closed);
     }
 }
